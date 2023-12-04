87c1450d9f1eb22adc17b9b2cabc2c828714ccd6
Sequoia Boubion-Mckay <111154276+Sequoiabm@users.noreply.github.com>
2023-12-04 00:25:28 -0800

    Finished Earthquake map
    map of all the worlds earthquakes that were found in the last week.
    includes:
    1. CSS for legend styling
    2. Leaflet map
    3. connects to D3 API
    4. maps JSON data from documentation in the READ.me

---
+++ EarthquakeCode/static/js/logic.js
@@ -0,0 +1,102 @@
+
+let basemap = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+        attribution: 'Map data: &copy;'
+    });
+
+let map = L.map("map", {
+    center: [40.7, -94.5],
+    zoom: 3
+})
+
+
+basemap.addTo(map);
+
+function getColor(depth){
+    if(depth > 90){
+        return "#ea2c2c"
+    }
+    else if(depth > 70){
+        return "#ea822c"
+    }
+    else if(depth > 50){
+        return "#ee9c00"
+    }
+    else if(depth > 30){
+        return "#eecc00"
+    }
+    else if(depth > 10){
+        return "#d4ee00"
+    }
+    else{
+        return "#98ee00"
+    }
+}
+
+function getRadius(magnitude){
+    if(magnitude === 0){
+        return 1
+    }
+    return magnitude * 4
+}
+
+// create a function
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data){
+    console.log(data);
+    function styleInfo(feature) {
+        return{
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius:getRadius(feature.properties.mag),
+            stroke: true, 
+            weight: 0.6
+        }
+    }
+
+    L.geoJson(data, {
+        pointToLayer: function(feature, latLng){
+            return L.circleMarker(latLng);
+        },
+        style: styleInfo,
+        onEachFeature: function(feature, layer){
+
+            // Convert time property to dd/mm/yy
+            const date = new Date(feature.properties.time);
+
+             // get day, month, and year
+            const day = date.getDate();
+            const month = date.getMonth(); // Months are zero-based, so we add 1
+            const year = date.getFullYear();
+
+            // format the date as "dd/mm/yy"
+            const formattedDate = `${day}/${month}/${year}`;
+           
+            // display all of the card information from JSON
+            layer.bindPopup(`
+                Magnitude: ${feature.properties.mag} <br>
+                Depth: ${feature.geometry.coordinates[2]} <br>
+                Location: ${feature.properties.place} <br>
+                Date: ${formattedDate}
+            `);
+        }
+    }).addTo(map);
+
+    let legend = L.control({
+        position: "bottomright"
+    });
+
+    legend.onAdd = function(){
+        let container = L.DomUtil.create("div", "info legend");
+        let grades = [-10, 10, 30, 50, 70, 90]
+        let colors = ['#98ee00', '#d4ee00', '#eecc00', '#ee9c00', '#ea822c', '#ea2c2c']
+        for(let index = 0; index < grades.length; index++) {
+            container.innerHTML += `<i style="background: ${colors[index]}"></i>${grades[index]}+ <br>`
+        }
+        return container;
+    }
+
+    legend.addTo(map);
+
+})
